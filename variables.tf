5a0bb25f75dd0f6a9ee85f7d847564c30ff992a3
malini-kamalambal <malini.pk@gmail.com>
2023-07-26 15:08:32 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,3 +10,13 @@ variable "db_password" {
   description = "RDS root user password"
   sensitive   = true
 }
+
+variable "access_key" {
+  description = "AWS access key"
+  sensitive   = true
+}
+
+variable "secret_key" {
+  description = "AWS secret key"
+  sensitive   = true
+}
